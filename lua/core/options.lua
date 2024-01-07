98a586d3ceed5f9adeb413b7a38c645781d94895
xenomech <gokulsmenon227@gmail.com>
2024-01-08 01:12:50 +0530

    feat: add vim options

---
+++ lua/core/options.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+opt.termguicolors = true
+
+opt.number = true
+opt.relativenumber = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.autoindent = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+
+opt.hlsearch = false
+opt.incsearch = true
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
-local M = {{
-    "navarasu/onedark.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-        local theme = require("onedark")
-        theme.setup({
-            style = "darker"
-        })
-        theme.load()
-    end
-}}
+local M = {
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			local theme = require("onedark")
+			theme.setup({
+				style = "darker"
+			})
+			theme.load()
+		end
+	}
+}
 
 return M
 
