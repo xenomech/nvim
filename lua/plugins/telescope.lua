d4a320de922bc17c24702f78da8af46eea7fb746
xenomech <gokulsmenon227@gmail.com>
2024-01-08 01:45:13 +0530

    feat: add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,27 @@
+local keymap = vim.keymap
+
+local M = {
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      keymap.set("n", "<C-p>", builtin.find_files, {})
+      keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+}
+return M
