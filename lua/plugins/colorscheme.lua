56c00c3bb0e9f417043ef42909ee5307007b05d5
xenomech <gokulsmenon227@gmail.com>
2024-01-08 01:03:36 +0530

    feat: add colorSheme - onedark

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+local M = {{
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+        local theme = require("onedark")
+        theme.setup({
+            style = "darker"
+        })
+        theme.load()
+    end
+}}
+
+return M
+
