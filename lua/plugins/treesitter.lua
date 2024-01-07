98dec093a42de9cebb52972a036c5ae06ea62ed2
xenomech <gokulsmenon227@gmail.com>
2024-01-08 01:52:29 +0530

    feat: add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+local M = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"yaml",
+					"html",
+					"css",
+					"prisma",
+					"markdown",
+					"markdown_inline",
+					"graphql",
+					"bash",
+					"lua",
+					"vim",
+					"dockerfile",
+					"gitignore",
+				},
+			})
+		end
+	}
+}
+
+return M
