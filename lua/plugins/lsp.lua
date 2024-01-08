1eef39d163b255713eaf9e8a1ae66cbae94a5b51
xenomech <gokulsmenon227@gmail.com>
2024-01-08 08:29:33 +0530

    feat: add lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,43 @@
+local keymap = vim.keymap
+
+local M = {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+				capabilities = capabilities
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+		
+			keymap.set("n", "K", vim.lsp.buf.hover, {})
+			keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
+
+return M
